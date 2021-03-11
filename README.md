5b48f9760f99d34edf45af0a2843cf25ab3fa016
ollypom <opomer@amazon.co.uk>
2021-03-11 15:03:23 +0000

    Adding Application, Pipeline and Infrastructure
    Signed-off-by: ollypom <opomer@amazon.co.uk>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# demo-app-for-docker-compose
+# Demo App for Docker Compose
+
+This repository includes a sample application to be used in
+Docker Compose demonstrations, specifically demos involving the [Docker Compose
+for Amazon ECS Plugin](https://docs.docker.com/cloud/ecs-integration/)
+
+This repository also includes CloudFormation templates to:
+- Build the foundational AWS resources (VPCs, ECS Clusters).
+- Build a CodePipeline to deploy the Docker Compose file to Amazon ECS in an
+  automated fashion.
---
+++ application/frontend/myweb/app.py
@@ -0,0 +1,65 @@
+from datetime import date
+from flask import Flask, render_template, request, g
+import json
+import os
+import socket
+import pandas as pd
+from redis import Redis
+
+app = Flask(__name__)
+
+redisurl = os.getenv('REDIS_URL')
+hostname = socket.gethostname()
+
+# Button Colour
+buttoncolour = "blue"
+button = './static/{}.png'.format(buttoncolour)
+
+def get_redis():
+    if not hasattr(g, 'redis'):
+        g.redis = Redis(host=redisurl, db=0, socket_timeout=5, decode_responses=True)
+    return g.redis
+
+def generate_table():
+    redis = get_redis()
+    result = pd.DataFrame(columns=['Date', 'Clicks'])
+
+    keys = redis.keys('*')
+    for key in keys:
+        val = redis.get(key)
+        raw = json.dumps([{'Date': key, 'Clicks': val}])
+        df = pd.read_json(raw)
+        result = result.append(df, ignore_index=True)
+
+    result = result.sort_values(by=['Date'], ascending=False)
+
+    return result
+
+@app.route('/', methods=['POST','GET'])
+def index():
+
+    redis = get_redis()
+    today = str(date.today())
+
+    if request.method == 'POST':
+        redis.incr(today)
+
+    global no_clicks
+    no_clicks = redis.get(today)
+
+    df = generate_table()
+
+    return render_template(
+       'index.html',
+       no_clicks=no_clicks,
+       hostname=hostname,
+       logo=button,
+       tables=[df.to_html(classes='data', index=False)],
+       titles=df.columns.values)
+
+@app.route('/health', methods=['GET'])
+def health():
+    return render_template('health.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
